132e22e9f78a9870d339daf4144a322d63da5522
jpalenic <jaroslav.palenicek@oracle.com>
2025-11-04 01:07:22 +0100

    Nextjs deploy

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // needed for GitHub Pages
+  },
+  basePath: '/jaraburningman', // 👈 replace with your repo name
+  assetPrefix: '/jaraburningman/',
+  distDir: 'build'
 };
 
 export default nextConfig;
