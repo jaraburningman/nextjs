e6111489675bff4cf0c097a79ff79cd8dcd1cd95
jpalenic <jaroslav.palenicek@oracle.com>
2025-11-05 01:00:46 +0100

    Updates

--- _next/static/Ep7gNkqLdxw2-6huYkGjT/_buildManifest.js
+++ _next/static/Ep7gNkqLdxw2-6huYkGjT/_buildManifest.js
@@ -3,8 +3,8 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/jaraburningman//_next/:path+",
-        "destination": "/jaraburningman/_next/:path+"
+        "source": "/nextjs//_next/:path+",
+        "destination": "/nextjs/_next/:path+"
       }
     ],
     "fallback": []
